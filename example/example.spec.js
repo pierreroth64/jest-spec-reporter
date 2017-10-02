afbf1ec59f483c0ef7194f073de5c22f515c2604
Peio Roth <pierreroth64@gmail.com>
2017-10-02 17:33:02 +0200

    No more failing test in example

--- example/example.spec.js
+++ example/example.spec.js
@@ -1,12 +1,6 @@
 describe('Jest Spec reporter', () => {
 
-  it('this is a passing test', () => {
-  });
+  it('this is a passing test', () => {});
 
-  it('this is a failing test', () => {
-    expect(false).toEqual(true);
-  });
-
-  it.skip('this is a pending test', () => {
-  });
+  it.skip('this is a pending test', () => {});
 });
