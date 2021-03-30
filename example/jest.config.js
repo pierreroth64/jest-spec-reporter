9c79fab89e58961f9090989c443d73871f4df274
Peio Roth <pierreroth64@gmail.com>
2021-03-30 14:50:36 +0200

    update dependencies

--- example/jest.config.js
+++ example/jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   verbose: false,
   testPathIgnorePatterns: ['/node_modules/', '/coverage/'],
   reporters: [
-    '../lib/jest-spec-reporter.js' // replace this line with 'jest-spec-reporter.js' in your real world config
-  ]
+    '../lib/jest-spec-reporter.js', // replace this line with 'jest-spec-reporter.js' in your real world config
+  ],
 };
